ffef01b01f58b64cd734bc6350c5f6cf8af83d04
Patrick Gonzalez <63878568+patgonz@users.noreply.github.com>
2020-12-15 11:15:48 -0500

    Minor text edit to test/verify deployment

--- src/App.js
+++ src/App.js
@@ -119,7 +119,7 @@ const App = () => {
     <ThemeProvider theme={theme}>
       <SearchDrawer searchDrawer={searchDrawer} dates={dates} setDates={setDates} />
       <Header appName='Data CRIB' />
-      <h2>Welcome to Amazon ECS</h2>
+      <h2>Welcome to Amazon ECS version</h2>
       <Home data={data} searchBar={searchBar} searchDrawer={searchDrawer} />
     </ThemeProvider>
   )
